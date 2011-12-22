a1ca08a1077620269fd10c6e011b09eae8993129
Mike Nolta <mike@nolta.net>
2011-12-22 15:27:57 -0500

    switch from hex to decimal floats in constants.h; add speed_of_light

--- src/actpol/make_constants_h.py
+++ src/actpol/make_constants_h.py
@@ -5,32 +5,31 @@ import math
 HEADER = """#pragma once
 """
 
-def s(x):
-    return "%g" % x
-
-def h(x):
-    return x.hex()
+def d(x):
+    return repr(x)
 
 def main():
 
     latitude_deg = -22.9585
     longitude_east_deg = -67.7876
     elevation_m = 5188.0
+    speed_of_light = 299792458.
 
     d2r = math.pi/180
     latitude = latitude_deg*d2r
     longitude_east = longitude_east_deg*d2r
 
     defines = [
-        ("ACTPOL_LATITUDE_DEG", s(latitude_deg)),
-        ("ACTPOL_LONGITUDE_EAST_DEG", s(longitude_east_deg)),
-        ("ACTPOL_ELEVATION_METERS", s(elevation_m)+"."),
-        ("ACTPOL_LATITUDE", h(latitude)),
-        ("ACTPOL_LONGITUDE_EAST", h(longitude_east)),
-        ("ACTPOL_SIN_LATITUDE", h(math.sin(latitude))),
-        ("ACTPOL_COS_LATITUDE", h(math.cos(latitude))),
-        ("ACTPOL_SIN_LONGITUDE_EAST", h(math.sin(longitude_east))),
-        ("ACTPOL_COS_LONGITUDE_EAST", h(math.cos(longitude_east))),
+        ("ACTPOL_LATITUDE_DEG", d(latitude_deg)),
+        ("ACTPOL_LONGITUDE_EAST_DEG", d(longitude_east_deg)),
+        ("ACTPOL_ELEVATION_METERS", d(elevation_m)),
+        ("SPEED_OF_LIGHT_M_PER_S", d(speed_of_light)),
+        ("ACTPOL_LATITUDE", d(latitude)),
+        ("ACTPOL_LONGITUDE_EAST", d(longitude_east)),
+        ("ACTPOL_SIN_LATITUDE", d(math.sin(latitude))),
+        ("ACTPOL_COS_LATITUDE", d(math.cos(latitude))),
+        ("ACTPOL_SIN_LONGITUDE_EAST", d(math.sin(longitude_east))),
+        ("ACTPOL_COS_LONGITUDE_EAST", d(math.cos(longitude_east))),
     ]
 
     f = open("constants.h", "w")
