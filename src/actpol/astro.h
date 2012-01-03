02136445c78fbbc278e6b63833713307a5011d84
Mike Nolta <mike@nolta.net>
2012-01-03 17:20:23 -0500

    add rad2arcsec

--- src/actpol/astro.h
+++ src/actpol/astro.h
@@ -21,6 +21,7 @@ static inline double secs2days( double s ) { return s/86400.; }
 static inline double deg2rad( double deg ) { return deg*M_PI/180.; }
 static inline double rad2deg( double rad ) { return rad*180./M_PI; }
 static inline double arcsec2rad( double sec ) { return sec*M_PI/(180*3600); }
+static inline double rad2arcsec( double rad ) { return rad*180*3600/M_PI; }
 
 static inline void
 actpol_ang2vec(double a, double d, double r[3])
