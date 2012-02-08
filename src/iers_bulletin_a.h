dae2a793366298d972d126538cc7478cdc73b105
Mike Nolta <mike@nolta.net>
2012-02-08 14:02:44 -0500

    oops, should have been part of last commit

--- src/iers_bulletin_a.h
+++ src/iers_bulletin_a.h
@@ -6,7 +6,7 @@ extern "C" {
 #endif
 
 int
-get_iers_bulletin_a( double mjd, double *dut1, double *x, double *y );
+actpol_get_iers_bulletin_a( double mjd, double *dut1, double *x, double *y );
 
 #ifdef __cplusplus
 }
